d7a90e7bcc8c080ad93239115b23b1c3e26335b0
LTurns <lizzieturney@gmail.com>
2020-10-11 14:16:37 +0100

    added navigation

---
+++ layouts/default.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <!-- <v-btn
+        icon
+        @click.stop="rightDrawer = !rightDrawer"
+      > -->
+        <!-- <v-icon>mdi-menu</v-icon>
+      </v-btn> -->
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-footer>
+      <span> {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+           {
+          title: 'CBS Products',
+          to: '/'
+        },
+        {
+          title: 'Products',
+          to: '/products'
+        },
+        {
+          title: 'News',
+          to: '/news'
+        }, 
+         {
+          title: 'About Us',
+          to: '/about'
+        }, 
+         {
+          title: 'Where to Buy',
+          to: '/buy'
+        }, 
+          {
+          title: 'Get in Touch',
+          to: '/contact'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'CBS Products'
+    }
+  }
+}
+</script>
